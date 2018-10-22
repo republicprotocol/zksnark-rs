c0e47d9e91fd4e1f68f17f906c0180405113500b
ross <ross.pure@gmail.com>
2018-10-22 13:56:23 +1100

    groth16/circuit/builder_test: or gate tests passing

--- src/groth16/circuit/builder/tests.rs
+++ src/groth16/circuit/builder/tests.rs
@@ -50,3 +50,24 @@ fn and_test() {
         assert!(circuit.evaluate(and) == Z251::from(*l_and_r));
     }
 }
+
+#[test]
+fn or_test() {
+    let logic_table = [
+        (0, 0, 0),
+        (0, 1, 1),
+        (1, 0, 1),
+        (1, 1, 1),
+    ];
+
+    for (l, r, l_or_r) in logic_table.iter() {
+        let mut circuit = Circuit::<Z251>::new();
+        let l_wire = circuit.new_wire();
+        let r_wire = circuit.new_wire();
+        let or = circuit.new_or(l_wire, r_wire);
+
+        circuit.set_value(l_wire, Z251::from(*l));
+        circuit.set_value(r_wire, Z251::from(*r));
+        assert!(circuit.evaluate(or) == Z251::from(*l_or_r));
+    }
+}
