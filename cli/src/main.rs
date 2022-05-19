5c50e9d1ab7df33186fb193b85e6c106593f673b
Peter O'Leary <pete@timelight.com>
2022-05-19 15:56:37 -0700

    Binary serialization is working

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -8,7 +8,10 @@ use zksnark::{CoefficientPoly, ASTParser, QAP, FrLocal, TryParse, SigmaG1, Sigma
 use zksnark::groth16::fr::{G1Local, G2Local};
 
 extern crate rustc_serialize;
-use self::rustc_serialize::json;
+extern crate bincode;
+
+use bincode::SizeLimit::Infinite;
+use bincode::rustc_serialize::{encode, decode};
 
 /// Search for a pattern in a file and display the lines that contain it.
 #[derive(Debug, Parser)]
@@ -35,7 +38,7 @@ enum Commands {
     }
 }
 
-fn do_output(output_path: Option<std::path::PathBuf>, output_string: String) {
+fn do_string_output(output_path: Option<std::path::PathBuf>, output_string: String) {
 
     let mut out_writer = match output_path {
         Some(x) => {
@@ -47,13 +50,27 @@ fn do_output(output_path: Option<std::path::PathBuf>, output_string: String) {
     out_writer.write_all(output_string.as_bytes());
 }
 
+fn do_binary_output(output_path: std::path::PathBuf,  buf: Vec<u8>) -> io::Result<File> {
+    let mut file = File::create(&output_path).unwrap();
+    file.write_all(&buf);
+    return file
+}
+
+fn read_setup_file<'a>(setup_path: std::path::PathBuf) -> SetupFile {
+    let setup_bin = &*::std::fs::read(setup_path).unwrap();
+    return decode(setup_bin).unwrap();
+}
+
+const CHECK: u32 = 0xABAD1DEA;
+
 #[derive(RustcDecodable, RustcEncodable)]
 struct SetupFile {
+    check: u32,
     sigmag1: SigmaG1<G1Local>,
     sigmag2: SigmaG2<G2Local>
 }
 
-fn setup(zk_path: std::path::PathBuf, output_path: Option<std::path::PathBuf>) {
+fn setup(zk_path: std::path::PathBuf, output_path: std::path::PathBuf) {
 
     let code = &*::std::fs::read_to_string(zk_path).unwrap();
     let qap: QAP<CoefficientPoly<FrLocal>> = ASTParser::try_parse(code).unwrap().into();
@@ -63,16 +80,11 @@ fn setup(zk_path: std::path::PathBuf, output_path: Option<std::path::PathBuf>) {
 
     let (sigmag1, sigmag2) = zksnark::groth16::setup(&qap);
 
-    let setup_file_object = SetupFile {sigmag1: sigmag1, sigmag2: sigmag2};
+    let setup_file_object = SetupFile {check: CHECK, sigmag1: sigmag1, sigmag2: sigmag2};
 
-    do_output(output_path, json::encode(&setup_file_object).unwrap());
-}
-
-fn read_setup_file<'a>(setup_path: std::path::PathBuf) -> Result<SetupFile, self::rustc_serialize::json::DecoderError> {
-    let setup_json = &*::std::fs::read_to_string(setup_path).unwrap();
-    let setup: Result< SetupFile, self::rustc_serialize::json::DecoderError> = json::decode(setup_json);
-
-    return setup;
+    // do_string_output(output_path, json::encode(&setup_file_object).unwrap());
+    let encoded =  encode(&setup_file_object, Infinite).unwrap();
+    do_binary_output(output_path, encoded);
 }
 
 fn proof(setup_path: std::path::PathBuf, output_path: Option<std::path::PathBuf>) {
@@ -84,7 +96,7 @@ fn main() {
     let args = Cli::parse();
 
     match args.command {
-        Commands::Setup { zk_path, output_path }  => setup(zk_path.unwrap(), output_path),
+        Commands::Setup { zk_path, output_path }  => setup(zk_path.unwrap(), output_path.unwrap()),
         Commands::Proof { setup_path, output_path }  => proof(setup_path.unwrap(), output_path),
         _ => println!("unknown command!"),
     }
@@ -97,7 +109,13 @@ mod tests {
     use std::path::PathBuf;
 
     #[test]
+    fn try_setup_test() {
+        setup(PathBuf::from("../test_programs/simple.zk"), PathBuf::from("simple.setup.bin"));
+    }
+
+    #[test]
     fn try_read_setup_test() {
-        let result = read_setup_file(PathBuf::from("simple.json"));
-        assert!(!result.is_err(), "setup file did not parse")
-    }}
+        let result = read_setup_file(PathBuf::from("simple.setup.bin"));
+        assert!(result.check == CHECK)
+    }
+}
